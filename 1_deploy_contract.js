c533ed96be9fa285455c537aa15de7115a0854a6
Manish Kumar Dixit <manish.22scse1012417@galgotiasuniversity.edu.in>
2025-01-21 10:43:17 +0530

    Delete 1_deploy_contract.js

--- 1_deploy_contract.js
+++
@@ -1,7 +0,0 @@
-
-const Voting = artifacts.require("Voting");
-
-module.exports = function (deployer) {
-  const candidates = ["Alice", "Bob", "Charlie"]; // Example candidates
-  deployer.deploy(Voting, candidates);
-};
